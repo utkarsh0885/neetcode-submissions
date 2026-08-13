fd17c50851137c04dbb5d641a386c6e9eea7e2dd
Utkarsh Singh <e23cseu0885@bennett.edu.in>
2026-08-13 20:53:38 +0530

    Add: buy-and-sell-crypto - submission-1

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-1.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int minprice = INT_MAX;
+        int maxprofit = 0;
+        for(int price : prices) {
+            minprice = min(price,minprice);
+            maxprofit = max(maxprofit,price-minprice);
+        }
+        return maxprofit;
+    }
+};
