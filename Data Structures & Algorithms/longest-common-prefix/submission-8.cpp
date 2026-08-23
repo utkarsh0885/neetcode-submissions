e5fd76c46303988341ade99192be6ee0f9594b8e
Utkarsh Singh <e23cseu0885@bennett.edu.in>
2026-08-23 21:54:47 +0530

    Add: longest-common-prefix - submission-8

---
+++ Data Structures & Algorithms/longest-common-prefix/submission-8.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        sort(strs.begin(),strs.end());
+        string first = strs[0];
+        string last = strs[strs.size()-1];
+        int i = 0;
+        while(i < first.length() && i < last.length() && first[i] == last[i]) {
+            i++;
+        }
+        return first.substr(0,i);
+    }
+};
