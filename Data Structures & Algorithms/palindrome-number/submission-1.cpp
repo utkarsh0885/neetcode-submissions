53d628b69de66e12ec82ed545fb32557a46b1b30
Utkarsh Singh <e23cseu0885@bennett.edu.in>
2026-08-28 17:54:41 +0530

    Add: palindrome-number - submission-1

---
+++ Data Structures & Algorithms/palindrome-number/submission-1.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if (x < 0) 
+        return false;
+
+        int original = x;
+        int rev = 0;
+
+        while(x > 0) {
+            int digit = x % 10;
+            rev = 10 * rev + digit;
+            x = x / 10;
+        }
+        return original == rev;
+    }
+};
